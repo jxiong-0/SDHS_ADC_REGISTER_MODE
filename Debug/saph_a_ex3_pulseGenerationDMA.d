8ce6a77a7b40ab1885c1ae799e52c3b0d3b4b767
jxiong-0 <avogadro6022x@gmail.com>
2025-03-21 13:52:24 -0500

    add files

---
+++ Debug/saph_a_ex3_pulseGenerationDMA.d
@@ -0,0 +1,174 @@
+# FIXED
+
+saph_a_ex3_pulseGenerationDMA.obj: ../saph_a_ex3_pulseGenerationDMA.c
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/driverlib.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/inc/hw_memmap.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/ti/ccs2002/ccs/ccs_base/msp430/include/msp430.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/ti/ccs2002/ccs/ccs_base/msp430/include/msp430fr6043.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/ti/ccs2002/ccs/ccs_base/msp430/include/in430.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/ti/ccs2002/ccs/ccs_base/msp430/include/legacy.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/esi.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/eusci_b_i2c.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/cs.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/aes256.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/uups.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/framctl_a.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/timer_b.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/framctl.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/eusci_a_spi.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/eusci_a_uart.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/tlv.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/timer_a.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/pmm.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/adc12_b.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/sysctl.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/sfr.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/ref_a.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/mpy32.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/wdt_a.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/saph.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/crc32.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/mpu.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/mtif.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/lcd_c.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/rtc_c.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/gpio.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/rtc_b.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/comp_e.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/ram.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/saph_a.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/dma.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/hspll.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/eusci_b_spi.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/sdhs.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/crc.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdio.h
+saph_a_ex3_pulseGenerationDMA.obj: C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdarg.h
+
+../saph_a_ex3_pulseGenerationDMA.c:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/driverlib.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/inc/hw_memmap.h:
+
+C:/ti/ccs2002/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs2002/ccs/ccs_base/msp430/include/msp430fr6043.h:
+
+C:/ti/ccs2002/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/ti/ccs2002/ccs/ccs_base/msp430/include/legacy.h:
+
+C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h:
+
+C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
+
+C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
+
+C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h:
+
+C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h:
+
+C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h:
+
+C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h:
+
+C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/esi.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/eusci_b_i2c.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/cs.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/aes256.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/uups.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/framctl_a.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/timer_b.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/framctl.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/eusci_a_spi.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/eusci_a_uart.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/tlv.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/timer_a.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/pmm.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/adc12_b.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/sysctl.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/sfr.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/ref_a.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/mpy32.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/wdt_a.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/saph.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/crc32.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/mpu.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/mtif.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/lcd_c.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/rtc_c.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/gpio.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/rtc_b.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/comp_e.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/ram.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/saph_a.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/dma.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/hspll.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/eusci_b_spi.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/sdhs.h:
+
+C:/Users/avoga/workspace_ccstheia/saph_a_ex3_pulseGenerationDMA/driverlib/MSP430FR5xx_6xx/crc.h:
+
+C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdio.h:
+
+C:/ti/ccs2002/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdarg.h:
+
---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430fr6043.cmd 
+
+C_SRCS += \
+../saph_a_ex3_pulseGenerationDMA.c 
+
+C_DEPS += \
+./saph_a_ex3_pulseGenerationDMA.d 
+
+OBJS += \
+./saph_a_ex3_pulseGenerationDMA.obj 
+
+OBJS__QUOTED += \
+"saph_a_ex3_pulseGenerationDMA.obj" 
+
+C_DEPS__QUOTED += \
+"saph_a_ex3_pulseGenerationDMA.d" 
+
+
